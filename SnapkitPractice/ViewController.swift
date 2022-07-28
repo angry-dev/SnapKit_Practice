a680618f7548745305dfd1fe690b9cd9f34dd660
angry-dev <angrydev@naver.com>
2022-07-28 19:06:45 +0900

    Initial Commit

---
+++ SnapkitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapkitPractice
+//
+//  Created by 전혜성 on 2022/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
